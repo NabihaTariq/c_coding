96662b5ca666ce1370139ad666fd9e632c79c208
NabihaTariq <nabiha.tariq01@gmail.com>
2022-05-23 11:24:03 -0700

    Added c code for addition of two numbers entered by user

---
+++ addition_of_two_int.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{   int a;
+    int b; 
+    int sum;
+	printf("enter a number of a:");
+	scanf("%d", &a);
+	printf("enter a number of b:");
+	scanf("%d", &b);
+	sum=a+b;
+	printf("the value of sum is %d.", sum);
+}
