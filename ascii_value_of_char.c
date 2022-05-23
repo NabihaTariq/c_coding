8dc17cf1bdeda3875f7a451bea71a7767f88effe
NabihaTariq <nabiha.tariq01@gmail.com>
2022-05-23 11:30:23 -0700

    Added c code for finding ASCII value of a character

---
+++ ascii_value_of_char.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{   float a;
+    float b; 
+    float result;
+	printf("enter a number of a:");
+	scanf("%f", &a);
+	printf("enter a number of b:");
+	scanf("%f", &b);
+	result=a*b;
+	printf("the result is %f.", result);
+}
