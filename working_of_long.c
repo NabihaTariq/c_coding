1ab919792009ac1118305057a8a2abde28e17c21
NabihaTariq <nabiha.tariq01@gmail.com>
2022-05-23 11:38:05 -0700

    Added c code for long datatype

---
+++ working_of_long.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	float b;
+	double c;
+	char d;
+	printf("The size of int is %lu\n", sizeof(a));
+	printf("The size of float is %lu\n", sizeof(b));
+	printf("The size of double is %lu\n", sizeof(c));
+	printf("The size of char is %lu\n", sizeof(d));
+	
+}
